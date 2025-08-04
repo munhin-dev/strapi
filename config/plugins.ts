c24df85d3f4991a3a6ef42a2fc1657ca6a2252f7
mun hin <munhin.ooi@meyzerbusiness.com>
2025-08-04 13:37:52 +0800

    Add email provider configuration using Nodemailer with environment variables

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: '@strapi/provider-email-nodemailer',
+      providerOptions: {
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+          user: env('GMAIL_USER'),
+          pass: env('GMAIL_APP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: env('GMAIL_USER'),
+        defaultReplyTo: env('GMAIL_USER'),
+      },
+    },
+  },
+});
